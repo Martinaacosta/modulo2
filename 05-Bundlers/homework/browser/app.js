946f8f6af9710cdc42a5d8e5ae3eafd225263ac7
Martina <martinaa@medicarw.com.ar>
2022-08-23 10:54:26 -0300

    paint

--- 05-Bundlers/homework/browser/app.js
+++ 05-Bundlers/homework/browser/app.js
@@ -1,29 +1,30 @@
-(function () {
 
-  var whiteboard = window.whiteboard;
-  var socket = window.io(window.location.origin);
+import whiteboard from './whiteboard';
 
-  socket.on('connect', function () {
-    console.log('Connected!');
-  });
+import io from "socket.io-client";
 
-  socket.on('load', function (strokes) {
+var socket = io(window.location.origin)
 
-    strokes.forEach(function (stroke) {
-      var start = stroke.start;
-      var end = stroke.end;
-      var color = stroke.color;
-      whiteboard.draw(start, end, color, false);
-    });
+socket.on('connect', function () {
+  console.log('Connected!');
+});
 
-  });
-
-  socket.on('draw', function (start, end, color) {
+socket.on('load', function (strokes) {
+  strokes.forEach(function (stroke) {
+    var start = stroke.start;
+    var end = stroke.end;
+    var color = stroke.color;
     whiteboard.draw(start, end, color, false);
   });
 
-  whiteboard.on('draw', function (start, end, color) {
-    socket.emit('draw', start, end, color);
-  });
+});
 
-})();
+socket.on('draw', function (start, end, color) {
+  whiteboard.draw(start, end, color, false);
+});
+
+whiteboard.on('draw', function (start, end, color) {
+  socket.emit('draw', start, end, color);
+});
+
+
