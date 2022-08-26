05db78c17c0a4d917fd72a2b11bcabdec45c8fb9
Martina <martinaa@medicarw.com.ar>
2022-08-26 09:33:53 -0300

    estilo7

--- 07-React-Estilos/homework/src/components/Card.jsx
+++ 07-React-Estilos/homework/src/components/Card.jsx
@@ -1,6 +1,38 @@
 import React from 'react';
+import styles from './Card.module.css'
+import { IoCloseCircleOutline } from 'react-icons/io5';
 
 export default function Card(props) {
+  const { max, min, name, img, onClose, primary} = props
   // acá va tu código
-  return <div>Card Component</div>
+  return (
+    <div className={`${styles.card} ${props.primary ? styles.primary : ''}`}>
+      
+      <span className={styles.name}>
+        {name}
+        { !primary && <button className={styles.button} onClick={onClose}>
+          <IoCloseCircleOutline />
+          </button>}
+        </span>
+      <img
+        src={`http://openweathermap.org/img/wn/${img}@2x.png`}
+        alt="Icono de imagen"
+      />
+      <div className={styles.temps}>
+        <Temp label='Min' temp={min} />
+        <Temp label='Max' temp={max} />
+      </div>
+
+    </div>
+  )
 };
+
+
+function Temp({ label, temp }) {
+  return (
+    <div className={styles.temp}>
+      <span>{label}</span>
+      <span>{temp}</span>
+    </div>
+  )
+}
